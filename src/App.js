a72ce4c38f793ccba63925752afff49b2b9583fc
Mushe <mosescolince161@gmail.com>
2024-09-30 15:49:02 +0200

    nothing here

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Dashboard from './components/Dashboard';
 import AdminDashboard from './Admin/AdminDashboard';
 import AdminRegister from './Admin/Adminregister';
 import AdminLogin from './Admin/AdminLogin';
+import AddRoom from './Rooms/AddRoom';
+import RoomList from './Rooms/RoomList';
 import PrivateRoute from './components/PrivateRoute';
 import ProtectedRoute from './components/ProtectedRoute';
 
@@ -16,6 +18,8 @@ function App() {
         <Route path="/admin" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
         <Route path="/admin-register" element={<AdminRegister />} />
         <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/add-room" element={<AddRoom />} />
+        <Route path="/room-list" element={<RoomList />} />
       </Routes>
     </Router>
   );
